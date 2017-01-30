d86ea5f6f1c1a0aaa6199282cdb446a6cd6c092b
greicens <gsilva@mail.ccsf.edu>
2017-01-29 17:19:53 -0800

    adds a user show page

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get '/users' => 'users#index'
   get '/users/new' => 'users#new', as: 'new_user'
   post '/users' => 'users#create'
+  get '/users/:id' => 'users#show', as: 'user'
 end
