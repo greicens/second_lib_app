b48fbc1f863531d520dc2f9c52cb6b0ae72fd4e7
greicens <gsilva@mail.ccsf.edu>
2017-01-29 16:57:45 -0800

    adds a view for new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    user = User.create(user_params)
+  end
+
+  private
+  def user_params
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'users#index'
 
   get '/users' => 'users#index'
-  get '/users/new' => 'users#new', as: new_user
+  get '/users/new' => 'users#new', as: 'new_user'
 end
