861d964180bb47eff371d6ae9d3182a018a288c9
greicens <gsilva@mail.ccsf.edu>
2017-01-29 17:11:42 -0800

    adds a post route for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create(user_params)
+    @user = User.create(user_params)
+    redirect_to root_path
   end
 
   private
   def user_params
-    
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   get '/users' => 'users#index'
   get '/users/new' => 'users#new', as: 'new_user'
+  post '/users' => 'users#create'
 end
