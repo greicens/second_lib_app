1e515f5b4b674aa55745c22eb5ffe4f4b9880892
greicens <gsilva@mail.ccsf.edu>
2017-01-29 17:37:09 -0800

    adds sessions routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get '/users/new' => 'users#new', as: 'new_user'
   post '/users' => 'users#create'
   get '/users/:id' => 'users#show', as: 'user'
+  get '/login' => 'sessions#new', as: 'login'
+  post '/sessions' => 'sessions#create', as: 'sessions'
 end
